32ad56514864a7392cb496ac00c40eeda72a6d1c
Molodoy2003 <danik.bobko@bk.ru>
2024-07-05 16:40:25 +0300

    test

--- hw4-multi-language/src/App.tsx
+++ hw4-multi-language/src/App.tsx
@@ -4,16 +4,16 @@ import Home from './components/Home'
 import LanguageContext from './i18n/LanguageContext'
 
 function App() {
-	return (
-		<LanguageContext>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/:lang/home' element={<Home />} />
-					<Route path='/' element={<Navigate to='/en/home' />} />
-				</Routes>
-			</BrowserRouter>
-		</LanguageContext>
-	)
+  return (
+    <LanguageContext>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/:lang/home' element={<Home />} />
+          <Route path='/' element={<Navigate to='/en/home' />} />
+        </Routes>
+      </BrowserRouter>
+    </LanguageContext>
+  )
 }
 
 export default App
